9d6db100363074c8c57a1668ae41595e500eb6b3
Spencer C. Imbleau <spencer@imbleau.com>
2022-10-30 22:24:33 -0400

    Removed unused import

--- launchers/native/src/main.rs
+++ launchers/native/src/main.rs
@@ -1,7 +1,5 @@
-use std::io::Cursor;
-
 use bevy::{prelude::*, window::WindowId, winit::WinitWindows};
-use image;
+use std::io::Cursor;
 use winit::window::Icon;
 
 fn set_window_icon(windows: NonSend<WinitWindows>) {
