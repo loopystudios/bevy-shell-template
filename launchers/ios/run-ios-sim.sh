fc3c90180d536415bb3151a109d3a9d9b693d62e
Spencer C. Imbleau <spencer@imbleau.com>
2023-01-18 01:15:08 -0500

    fixed iOS build

--- launchers/ios/run-ios-sim.sh
+++ launchers/ios/run-ios-sim.sh
@@ -2,6 +2,7 @@
 set -e
 
 TARGET="${TARGET:-x86_64-apple-ios}"
+which dasel || brew install dasel
 APP_NAME="$(cat Cargo.toml | dasel -r toml '.package.name')"
 BUNDLE_ID="$(cat Cargo.toml | dasel -r toml '.package.metadata.bundle.identifier')"
 
@@ -10,7 +11,7 @@ BUNDLE_CMD="cargo bundle --target $TARGET"
 echo "Bundling app for iOS"
 which cargo-bundle || cargo install cargo-bundle
 $BUNDLE_CMD
-xcrun simctl boot "iPhone 12 mini" || true
+xcrun simctl boot "iPhone 14" || true
 open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app
 xcrun simctl install booted "target/$TARGET/debug/bundle/ios/$APP_NAME.app"
 xcrun simctl launch --console booted "$BUNDLE_ID"
