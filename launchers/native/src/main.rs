297b4018fb904866a041bc9b00e2258a70f78c96
Matias Kotlik <20362627+qmatias@users.noreply.github.com>
2023-05-03 08:29:25 -0500

    fix: update to bevy 0.10 api (#51)

--- launchers/native/src/main.rs
+++ launchers/native/src/main.rs
@@ -1,9 +1,16 @@
-use bevy::{prelude::*, window::WindowId, winit::WinitWindows};
+use bevy::{prelude::*, winit::WinitWindows};
 use std::io::Cursor;
+use bevy::window::PrimaryWindow;
 use winit::window::Icon;
 
-fn set_window_icon(windows: NonSend<WinitWindows>) {
-    let window = windows.get_window(WindowId::primary()).expect("no window");
+fn set_window_icon(
+    primary_query: Query<Entity, With<PrimaryWindow>>,
+    windows: NonSend<WinitWindows>,
+) {
+    let entity = primary_query.single();
+    let window = windows
+        .get_window(entity)
+        .expect("should have primary window");
     let (icon_rgba, icon_width, icon_height) = {
         let icon_buf = Cursor::new(include_bytes!("../static/appicon.png"));
         let rgba = image::load(icon_buf, image::ImageFormat::Png)
